c521c541d02ba15a5b05168c363946dc319ff6a5
Marcelo Serpa <boss@fullofcaffeine.com>
2026-02-18 21:25:15 -0600

    test(semantic): avoid filesystem side-effects in sys/io fixture

--- test/semantic_diff/sys_io_roundtrip/Main.hx
+++ test/semantic_diff/sys_io_roundtrip/Main.hx
@@ -1,14 +1,13 @@
 class Main {
 	static function main() {
-		var path = "./semdiff_tmp.txt";
-		sys.io.File.saveContent(path, "alpha\\nbeta\\n");
-
-		var data = sys.io.File.getContent(path);
-		Sys.println(data);
-
 		var p = new sys.io.Process("echo", ["hello-sem"]);
 		var line = p.stdout.readLine();
 		p.close();
 		Sys.println(line);
+
+		var p2 = new sys.io.Process("echo", ["a:b:c"]);
+		var line2 = p2.stdout.readLine();
+		p2.close();
+		Sys.println(line2);
 	}
 }
